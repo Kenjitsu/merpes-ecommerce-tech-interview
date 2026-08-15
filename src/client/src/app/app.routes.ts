8a6a82b169b6f08bf0676091fb6fee2d3a094e94
Ricardo Corredor <ricorga_97@hotmail.com>
2026-08-15 05:27:11 -0500

    Inicializando proyecto frontend ionic

---
+++ src/client/src/app/app.routes.ts
@@ -0,0 +1,33 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'catalog',
+    loadComponent: () => import('./pages/catalog/catalog.page').then( m => m.CatalogPage)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./pages/register/register.page').then( m => m.RegisterPage)
+  },
+  {
+    path: 'cart',
+    loadComponent: () => import('./pages/cart/cart.page').then( m => m.CartPage)
+  },
+  {
+    path: 'checkout',
+    loadComponent: () => import('./pages/checkout/checkout.page').then( m => m.CheckoutPage)
+  },
+];
