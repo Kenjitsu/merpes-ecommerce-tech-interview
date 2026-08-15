9ed6f9a6bb9516d1f794de339f2bdae932450b7c
Ricardo Corredor <ricorga_97@hotmail.com>
2026-08-15 07:56:43 -0500

    Add - primer diseño de vistas de catalogo y carrito funcional

--- src/client/src/app/pages/cart/cart.page.ts
+++ src/client/src/app/pages/cart/cart.page.ts
@@ -1,20 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton, IonIcon, IonButton, IonList, IonItem, IonLabel, IonThumbnail, IonText, IonFooter, IonRow, IonCol, IonBadge, IonGrid } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { trashOutline, addCircle, removeCircle, arrowBackOutline, cartOutline } from 'ionicons/icons';
+import { Cart } from '../../services/cart';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.page.html',
   styleUrls: ['./cart.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [ 
+    IonCol,
+    IonRow,
+    IonFooter,
+    IonText,
+    IonLabel,
+    IonItem,
+    IonThumbnail,
+    IonList,
+    IonButton,
+    IonIcon,
+    IonBackButton,
+    IonButtons,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    RouterLink
+  ],
 })
-export class CartPage implements OnInit {
+export class CartPage {
+  public cartService = inject(Cart);
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    addIcons({
+      trashOutline,
+      addCircle,
+      removeCircle,
+      arrowBackOutline,
+      cartOutline,
+    });
   }
 
+  increaseItemQuantity(productId: number) {
+    const item = this.cartService
+      .cartItems()
+      .find((i) => i.product.id === productId);
+    if (item) {
+      this.cartService.addToCart(item.product, 1);
+    }
+  }
+
+  decreaseItemQuantity(productId: number) {
+    this.cartService.decreaseQuantity(productId);
+  }
+
+  removeItem(productId: number) {
+    this.cartService.removeFromCart(productId);
+  }
+
+  proceedToCheckout() {
+    console.log('Iniciando proceso de pago...');
+  }
 }
--- src/client/src/app/pages/catalog/catalog.page.ts
+++ src/client/src/app/pages/catalog/catalog.page.ts
@@ -1,20 +1,89 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonBadge, IonButton, IonButtons, IonContent, IonHeader, IonIcon, IonTitle, IonToolbar, IonRow, IonGrid, IonCol, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonText } from '@ionic/angular/standalone';
+import { Item, Product } from '../../services/product';
+import { Cart } from '../../services/cart';
+import { addIcons } from 'ionicons';
+import { addCircle, removeCircle, cartOutline, personCircleOutline, addCircleOutline } from 'ionicons/icons';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-catalog',
   templateUrl: './catalog.page.html',
   styleUrls: ['./catalog.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonText,
+    IonCardContent,
+    IonCardSubtitle,
+    IonCardTitle,
+    IonCardHeader,
+    IonRow,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonButton,
+    IonButtons,
+    IonBadge,
+    IonIcon,
+    IonGrid,
+    IonCol,
+    IonCard,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    RouterLink,
+  ],
 })
 export class CatalogPage implements OnInit {
+  products: Item[] = [];
 
-  constructor() { }
+  productQuantities: Record<number, number> = {};
 
-  ngOnInit() {
+  private productService = inject(Product);
+  public cartService = inject(Cart);
+
+  constructor() {
+    addIcons({
+      addCircle,
+      removeCircle,
+      cartOutline,
+      personCircleOutline,
+      addCircleOutline,
+    });
   }
 
+  ngOnInit() {
+    this.products = this.productService.getProducts();
+
+    this.products.forEach((p) => {
+      this.productQuantities[p.id] = 1;
+    });
+  }
+
+  getItemQuantity(productId: number): number {
+    const item = this.cartService
+      .cartItems()
+      .find((i) => i.product.id === productId);
+    return item ? item.quantity : 0;
+  }
+
+  increaseQuantity(productId: number) {
+    this.productQuantities[productId]++;
+  }
+
+  decreaseQuantity(productId: number) {
+    if (this.productQuantities[productId] > 1) {
+      this.productQuantities[productId]--;
+    }
+    this.cartService.decreaseQuantity(productId);
+  }
+
+  addToCart(product: Item) {
+    const quantity = this.productQuantities[product.id];
+    this.cartService.addToCart(product, quantity);
+
+    this.productQuantities[product.id] = 1;
+  }
 }
